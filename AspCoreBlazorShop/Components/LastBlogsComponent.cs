a7f81914ca71efffd7c8e77b28bcb29515fd2ac4
mehran sattary <mehran999m9@gmail.com>
2023-05-11 22:18:33 +0430

    2023/05/11

---
+++ AspCoreBlazorShop/Components/LastBlogsComponent.cs
@@ -0,0 +1,28 @@
+﻿using Data.Context;
+using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspCoreBlazorShop.Components;
+
+public class LastBlogsComponent : ViewComponent
+{
+    private readonly AppDbContext context;
+    public LastBlogsComponent(AppDbContext context)
+    {
+        this.context = context;
+    }
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var lastBlogs =await context.Blogs.OrderByDescending(x=>x.CreateDate).Select(x=>
+            new Blog() 
+            {
+                BlogName=x.BlogName,
+                UpdateDate=x.UpdateDate,
+                Id=x.Id,
+                BlogImageSmall=x.BlogImageSmall
+                
+            }).Skip(0).Take(4).ToListAsync();
+        return View("/Views/Components/LastBlogsComponent.cshtml", lastBlogs);
+    }
+}
---
+++ AspCoreBlazorShop/Controllers/BlogController.cs
@@ -0,0 +1,30 @@
+﻿using Data.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using X.PagedList;
+
+namespace AspCoreBlazorShop.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly AppDbContext context;
+        public BlogController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        [Route("/Blogs")]
+        public IActionResult ShowBlogs(int currentpage = 1)
+        {
+
+            var blogs = context.Blogs.ToPagedList(currentpage, 12);
+            return View(blogs);
+        }
+
+        [Route("/Blog/{Id}")]
+        public IActionResult DetailBlog(int Id )
+        {
+            var blog = context.Blogs.FirstOrDefault(x=>x.Id==Id);
+            return View(blog);
+        }
+    }
+}
